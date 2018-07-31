5ebf554a9b5a9d6d8a03ef51e6b04d95bbc4ab44
ToxicFrazzles <frazzles740@gmail.com>
2018-07-31 21:18:14 +0100

    Started project

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+import os
+
+bot = commands.Bot(command_prefix='/')
+
+@bot.event
+async def on_ready():
+	print('Logged in as',bot.user.name)
+	print('-'*10)
+	bot.change_presence(game=discord.Game(name="Meh",type=2))
+
+@bot.command()
+async def roll(dice:str):
+	"""Rolls dice in NdN format"""
+	try:
+		rolls,limit = map(int, dice.split('d'))
+	except Exception:
+		await bot.say('Format has to be NdN!')
+		return
+	result = ', '.join(str(random.randint(1,limit)) for r in range(rolls))
+	await bot.say(result)
+
+if __name__=="__main__":
+	try:
+		with open("token.txt","r") as f:
+			token = f.read().rstrip()
+	except FileNotFoundError:
+		token = input("Client token: ")
+		with open("token.txt","w") as f:
+			f.write(token)
+	bot.run(token)
