c52277ded40577f98a6ba72a6d954e9865494b31
ToxicFrazzles <frazzles740@gmail.com>
2018-08-30 19:34:19 +0100

    updated audio.py

--- cogs/audio.py
+++ cogs/audio.py
@@ -8,14 +8,32 @@ class Audio:
 	"""Some audio commands"""
 	def __init__(bot):
 		self.bot=bot
-		
-	@commands.command
+		self.settings_file = "data/Audio/settings.json"
+		try:
+			with open(self.settings_file) as f:
+				self.settings = json.load(f)
+		except FileNotFoundError:
+			self.settings = {}
+			with open(self.settings_file,"w") as f:
+				json.dump(self.settings,f)
+	
+	def save_settings(self):
+		with open(self.settings_file,"w") as f:
+			json.dump(self.settings,f)
+	
+	@commands.group()
 	async def audioset(ctx):
-		pass
+		if not ctx.invoked_subcommand:
+			await ctx.send("This command only has subcommands and I don't know how to show you the help here")
+	
+	@audioset.command()
+	async def volume(ctx,volume:int):
+		self.settings[str(ctx.guild.id)]['volume'] = volume
+		self.save_settings()
 
 
 def check_folders():
-	folders = []
+	folders = ["data/Audio"]
 	for folder in folders:
 		if os.path.exists(folder) and os.isdir(folder):
 			continue
